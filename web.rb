e2dd0c0b0723eea5ee5571eac779eb9ba4d69782
Blockli <95665888+blocklienterprise@users.noreply.github.com>
2025-12-09 02:56:06 -0500

    Fix Sinatra host authorization syntax

--- web.rb
+++ web.rb
@@ -12,8 +12,8 @@ configure do
   # Additional settings for production deployment
   set :bind, '0.0.0.0'
   set :port, ENV.fetch('PORT', 4567)
-  # Disable host authorization protection completely for Render
-  set :protection, false
+  # Disable host authorization protection for Render deployment
+  set :protection, except: :host_authorization
 end
 
 before do
